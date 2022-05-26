86f090ba44290cf9406baba85bfdb0622a22c10d
Varinthorm <Jaitrong3016@gmail.com>
2022-05-26 10:28:55 +0700

    TC-002 success

---
+++ pythonlibs/utils.py
@@ -0,0 +1,15 @@
+from numpy.random import seed
+from numpy.random import randint
+import random
+
+  
+def generate_random_numbers():
+    for i in range(10):
+      values = random.randint(0, 1000000)
+
+    return values
+
+  
+  
+  
+  
